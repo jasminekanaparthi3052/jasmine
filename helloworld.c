7ffa9ed2cf7448c6d83216ee99cb0e77cb05aa3e
jasmine <user20@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-02 10:59:45 +0000

    helloworld

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/* This is a hello world Program
+   Owner : Jasmine Date: 02-0902024
+   Revision History
+  i 1. Name:Jasmine K Date: 02-09-2024 */
+
+
+  #include<stdio.h>
+
+  int main()
+  {
+
+  	printf("Hello World\n");
+    return 0;	
+  }
